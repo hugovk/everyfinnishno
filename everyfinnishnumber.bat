1e2aeb13212fd280a81fa664f7969282e901529e
Hugo <hugovk@users.noreply.github.com>
2018-11-12 10:42:47 +0200

    Update paths

--- everyfinnishnumber.bat
+++ everyfinnishnumber.bat
@@ -1,10 +1,14 @@
+set PYTHON=C:\Python27\python.exe
+set DROPBOX_BIN=E:\Users\hugovk\Dropbox\bin
+set DROPBOX_DATA=E:\Users\hugovk\Dropbox\bin\data
+set TEE=%DROPBOX_BIN%\UnxUtils\tee
 
 :loop
 
 @date/t && time/t
-@everyfinnishno.py -nw
+@%PYTHON% %DROPBOX_BIN%\everyfinnishno.py -nw
 @date/t && time/t
 
-@sleep 60 REM 1 mins (sleep.exe not on all WinXP)
+@%PYTHON% %DROPBOX_BIN%\sleep.py 60 REM 1 mins (sleep.exe not on all WinXP)
 
 goto loop
